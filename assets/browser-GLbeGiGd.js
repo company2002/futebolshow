0e220e2f8341d893dfefe52dfd85ee6a714fad05
company2002 <seu.email@exemplo.com>
2025-03-19 19:07:29 -0300

    Deploy to GitHub Pages

---
+++ assets/browser-GLbeGiGd.js
@@ -0,0 +1,1 @@
+import{F as e}from"./index-ykHg8W5u.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
